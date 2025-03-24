4574c119e82bd6ccf44b1d4dba5e912308639cce
Gareth Callanan <gcallanan@gmail.com>
2025-03-24 11:13:51 +0100

    Autoformatted code

--- lib/Fifo/FifoOps.cpp
+++ lib/Fifo/FifoOps.cpp
@@ -16,37 +16,47 @@ using namespace mlir::fifo;
 #define GET_OP_CLASSES
 #include "Fifo/FifoOps.cpp.inc"
 
+LogicalResult CreateOp::verify() {
+  if (getBufferSize() <= 0) {
+    return emitOpError() << "CreateOp buffer size is "
+                         << std::to_string(getBufferSize())
+                         << " but must be greater than 0";
+  }
 
-LogicalResult CreateOp::verify()
-{
-    if(getBufferSize() <= 0){
-        return emitOpError() << "CreateOp buffer size is " << std::to_string(getBufferSize()) <<" but must be greater than 0";
-    }
+  // I think that this error is picked up by the "parser", it may not be
+  // necessary to check it here
+  if (getResults().size() != 2) {
+    return emitOpError() << "CreateOp must have 2 results";
+  }
 
-    // I think that this error is picked up by the "parser", it may not be necessary to check it here
-    if(getResults().size() != 2){
-        return emitOpError() << "CreateOp must have 2 results";
-    }
+  Type inputPortType = getInputPort().getType();
+  if (!inputPortType.isa<InputPortType>()) {
+    return emitOpError() << "expected inputPort to be of type InputPortType "
+                            "(!fifo.input_port<"
+                         << getElementType() << ">), but got " << inputPortType;
+  }
 
-    Type inputPortType = getInputPort().getType();
-    if (!inputPortType.isa<InputPortType>()) {
-        return emitOpError() << "expected inputPort to be of type InputPortType (!fifo.input_port<"<< getElementType() <<">), but got " << inputPortType;
-    }
+  InputPortType inputPort = inputPortType.cast<InputPortType>();
+  if (inputPort.getElementType() != getElementType()) {
+    return emitOpError() << "expected inputPort element type to be "
+                         << getElementType() << ", but got "
+                         << inputPort.getElementType();
+  }
 
-    InputPortType inputPort = inputPortType.cast<InputPortType>();
-    if (inputPort.getElementType() != getElementType()) {
-        return emitOpError() << "expected inputPort element type to be " << getElementType() << ", but got " << inputPort.getElementType();
-    }
+  Type outputPortType = getOutputPort().getType();
+  if (!outputPortType.isa<OutputPortType>()) {
+    return emitOpError() << "expected outputPort to be of type OutputPortType "
+                            "(!fifo.output_port<"
+                         << getElementType() << ">), but got "
+                         << outputPortType;
+  }
 
-    Type outputPortType = getOutputPort().getType();
-    if (!outputPortType.isa<OutputPortType>()) {
-        return emitOpError() << "expected outputPort to be of type OutputPortType (!fifo.output_port<"<< getElementType() <<">), but got " << outputPortType;
-    }
+  OutputPortType outputPort = outputPortType.cast<OutputPortType>();
+  if (outputPort.getElementType() != getElementType()) {
+    return emitOpError() << "expected outputPort element type to be "
+                         << getElementType() << ", but got "
+                         << outputPort.getElementType();
+  }
 
-    OutputPortType outputPort = outputPortType.cast<OutputPortType>();
-    if (outputPort.getElementType() != getElementType()) {
-        return emitOpError() << "expected outputPort element type to be " << getElementType() << ", but got " << outputPort.getElementType();
-    }
-
-    return success();
+  return success();
 }
