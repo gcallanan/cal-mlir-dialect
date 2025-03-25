9a46132242f5c062b405a893f6bff3ac175720f2
Gareth Callanan <gcallanan@gmail.com>
2025-03-25 16:04:25 +0100

    Have an empty pass registered and replaced

--- cal-opt/cal-opt.cpp
+++ cal-opt/cal-opt.cpp
@@ -24,12 +24,13 @@ int main(int argc, char **argv) {
   // TODO: Register cal passes here.
 
   mlir::DialectRegistry registry;
-  registry.insert<mlir::cal::CalDialect, mlir::fifo::FifoDialect>();
-                  //mlir::arith::ArithDialect, mlir::func::FuncDialect>();
+  registry.insert<mlir::cal::CalDialect, mlir::fifo::FifoDialect,
+                  mlir::arith::ArithDialect, mlir::func::FuncDialect,
+                  mlir::memref::MemRefDialect>();
   // Add the following to include *all* MLIR Core dialects, or selectively
   // include what you need like above. You only need to register dialects that
   // will be *parsed* by the tool, not the one generated
-  registerAllDialects(registry);
+  //registerAllDialects(registry);
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "Cal optimizer driver\n", registry));
--- lib/Dialect/Fifo/FifoPasses.cpp
+++ lib/Dialect/Fifo/FifoPasses.cpp
@@ -9,14 +9,55 @@
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Rewrite/FrozenRewritePatternSet.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-
+#include "mlir/Transforms/DialectConversion.h"
+#include "Dialect/Fifo/FifoOps.h"
+#include "Dialect/Fifo/FifoDialect.h"
+#include "Dialect/Fifo/FifoTypes.h"
 #include "Dialect/Fifo/FifoPasses.h"
+#include <iostream>
 
 namespace mlir::fifo {
-#define GEN_PASS_DEF_FIFOSWITCHBARFOO
+#define GEN_PASS_DEF_LOWERFIFOTOMEMREFPASS
 #include "Dialect/Fifo/FifoPasses.h.inc"
 
-namespace {
+class ConvertCreateOpToMemref
+    : public OpConversionPattern<CreateOp> {
+  using OpConversionPattern<CreateOp>::OpConversionPattern;
 
-} // namespace
+  LogicalResult matchAndRewrite(CreateOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    std::cout << "We got here" << std::endl;
+    // if (op.getIsLegal())
+    //   return failure();
+    // rewriter.startOpModification(op);
+    // Block *body = &op.getBody().front();
+    // TypeConverter::SignatureConversion result(body->getNumArguments());
+    // for (auto it : llvm::enumerate(body->getArgumentTypes()))
+    //   result.addInputs(it.index(), {it.value(), it.value()});
+    // rewriter.applySignatureConversion(body, result, getTypeConverter());
+    // op.setIsLegal(true);
+    // rewriter.finalizeOpModification(op);
+
+    return success();
+  }
+};
+
+
+class LowerFifoToMemrefPass
+    : public impl::LowerFifoToMemrefPassBase<LowerFifoToMemrefPass> {
+public:
+  // using impl::LowerFifoToMemrefPassBase<
+  //  LowerFifoToMemrefPass>::LowerFifoToMemrefPassBase;
+  void runOnOperation() final {
+    ConversionTarget target(getContext());
+
+
+    RewritePatternSet patterns(&getContext());
+    patterns.add<ConvertCreateOpToMemref>(&getContext());
+
+    if (failed(applyPartialConversion(getOperation(), target, std::move(patterns))))
+        signalPassFailure();
+  }
+};
+
 } // namespace mlir::fifo
