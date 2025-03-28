5c5da9f658fb426197e9a4ab6297adc3a90b0aaf
Gareth Callanan <gcallanan@gmail.com>
2025-03-28 13:04:58 +0100

    Added a pass to decompose tuples.

--- lib/Dialect/Fifo/FifoPassesBypassTuples.cpp
+++ lib/Dialect/Fifo/FifoPassesBypassTuples.cpp
@@ -4,10 +4,18 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+// NOTE from @Gareth Callanan
+// This pass is copied almost exactly from:
+//    llvm-project/mlir/test/lib/Conversion/OneToNTypeConversion/TestOneToNTypeConversionPass.cpp
+// I needed 1:N argument conversions fo convert the fifo.input_port<> and fifo.output_port<>
+// types to memref types. When I looked at it, MLIR had just been updated to change the way things
+// were done and it was unclear how to make full use of this. Instead of trying to figure it out, I
+// just copied to test and modified it. 
 //===----------------------------------------------------------------------===//
-#include "Dialect/Fifo/FifoPasses.h"
+
 #include "Dialect/Fifo/FifoDialect.h"
 #include "Dialect/Fifo/FifoOps.h"
+#include "Dialect/Fifo/FifoPasses.h"
 #include "Dialect/Fifo/FifoTypes.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
@@ -17,40 +25,93 @@
 #include "mlir/Rewrite/FrozenRewritePatternSet.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "mlir/Transforms/OneToNTypeConversion.h"
 #include <iostream>
 
 namespace mlir::fifo {
 #define GEN_PASS_DEF_BYPASSFIFOTUPLES
 #include "Dialect/Fifo/FifoPasses.h.inc"
 
+class ConvertMakeTuple : public OneToNOpConversionPattern<MakeTuple> {
+public:
+  using OneToNOpConversionPattern<MakeTuple>::OneToNOpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(MakeTuple op, OpAdaptor adaptor,
+                  OneToNPatternRewriter &rewriter) const override {
+    // Simply replace the current op with the converted operands.
+    rewriter.replaceOp(op, adaptor.getFlatOperands(),
+                       adaptor.getResultMapping());
+    return success();
+  }
+};
+
+class ConvertGetTupleElement
+    : public OneToNOpConversionPattern<GetTupleElement> {
+  using OneToNOpConversionPattern<GetTupleElement>::OneToNOpConversionPattern;
+
+public:
+  LogicalResult
+  matchAndRewrite(GetTupleElement op, OpAdaptor adaptor,
+                  OneToNPatternRewriter &rewriter) const override {
+    // Construct mapping for tuple element types.
+    auto stateType = cast<TupleType>(op->getOperand(0).getType());
+    TypeRange originalElementTypes = stateType.getTypes();
+    OneToNTypeMapping elementMapping(originalElementTypes);
+    if (failed(typeConverter->convertSignatureArgs(originalElementTypes,
+                                                   elementMapping)))
+      return failure();
+
+    // Compute converted operands corresponding to original input tuple.
+    assert(adaptor.getOperands().size() == 1 &&
+           "expected 'get_tuple_element' to have one operand");
+    ValueRange convertedTuple = adaptor.getOperands()[0];
+
+    // Got those converted operands that correspond to the index-th element of
+    // the original input tuple.
+    size_t index = op.getIndex();
+    ValueRange extractedElement =
+        elementMapping.getConvertedValues(convertedTuple, index);
+
+    rewriter.replaceOp(op, extractedElement, adaptor.getResultMapping());
+
+    return success();
+  }
+};
+
+static void
+populateDecomposeTuplesTestPatterns(const TypeConverter &typeConverter,
+                                    RewritePatternSet &patterns) {
+  patterns.add<ConvertMakeTuple, ConvertGetTupleElement
+               // clang-format on
+               >(typeConverter, patterns.getContext());
+}
+
 class BypassFifoTuplesPass
     : public impl::BypassFifoTuplesBase<BypassFifoTuplesPass> {
 public:
-//    using impl::BypassFifoTuplesBase<
-//    BypassFifoTuplesBase>::BypassFifoTuplesBase;
+  // using impl::BypassFifoTuplesPass<
+  // BypassFifoTuplesBase>::BypassFifoTuplesPass;
   void runOnOperation() final {
-    ConversionTarget target(getContext());
+    auto *context = &getContext();
 
-    // Something about this being a stack
-    RewritePatternSet patterns(&getContext());
-    // patterns.add<ConvertFifoPullToMemref>(&getContext());
-    // patterns.add<ConvertFifoPushToMemref>(&getContext());
-    // patterns.add<ConvertFifoCreateOpToMemref>(&getContext());
+    // Assemble type converter.
+    TypeConverter typeConverter;
+    typeConverter.addConversion([](Type type) { return type; });
+    typeConverter.addConversion(
+        [](TupleType tupleType, SmallVectorImpl<Type> &types) {
+          tupleType.getFlattenedTypes(types);
+          return success();
+        });
 
-    // // Set the legal and illegal dialects after this conversion
-    // target.addIllegalDialect<fifo::FifoDialect>();
-    // target.addLegalOp<fifo::MakeTuple, fifo::GetTupleElement>();
-    // target.addLegalDialect<memref::MemRefDialect, index::IndexDialect,
-    //                        arith::ArithDialect>();
+    // Assemble patterns.
+    RewritePatternSet patterns(context);
+    populateDecomposeTuplesTestPatterns(typeConverter, patterns);
 
-    // Run the conversion
-    if (failed(applyPartialConversion(getOperation(), target,
-                                      std::move(patterns)))) {
-      std::cout << "Failed!!!" << std::endl;
-      signalPassFailure();
-    } else {
-      std::cout << "Success!!!" << std::endl;
-    }
+    // Run conversion.
+    if (failed(applyPartialOneToNConversion(getOperation(), typeConverter,
+                                            std::move(patterns))))
+      return signalPassFailure();
   }
 };
 
