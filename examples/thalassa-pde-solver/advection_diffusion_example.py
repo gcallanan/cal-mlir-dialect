b3a9b89e2726d79db210ab65cd1bbe824efcca36
Michail Boulasikis <michail.boulasikis@cs.lth.se>
2025-08-27 10:45:12 +0200

    Removed dt parameter from scripts and guaranteed stability.

--- examples/thalassa-pde-solver/advection_diffusion_example.py
+++ examples/thalassa-pde-solver/advection_diffusion_example.py
@@ -8,7 +8,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-torch', action='store_true', default=False, help='Use PyTorch backend instead of MLIR. This is not supported in this repo, so we do not guarentee that it works.')
 parser.add_argument('-torch-target', choices=['cpu', 'gpu'], default='cpu', help='Select PyTorch target device (CPU or GPU).')
 parser.add_argument('--hypercube-size', type=int, default=1000000, help='Size of the spatial discretization hypercube (default: 1000000)')
-parser.add_argument('--dt', type=float, default=0.00001, help='dt, neds to be less than 0.5dx^2 for stability (default: 0.00001)')
 parser.add_argument('--iterations', type=int, default=250, help='Number of time iterations to run (default: 250)')
 args = parser.parse_args()
 
@@ -35,7 +34,7 @@ disc = [
     thalassa.fdm_simple_partial_derivative(D(u, (x, 1)), dx, method='backward'),
     thalassa.fdm_simple_partial_derivative(D(u, (x, 2)), dx, method='central')
 ]
-dt = args.dt # Needs to be less than 0.5dx^2 for stability
+dt = 0.5 * (0.5 * dx * dx) # Guaranteed to be less than 0.5dx^2 for stability
 
 # Step 2: Compile the PDE system to MLIR or PyTorch
 hypercube_size = args.hypercube_size
@@ -52,9 +51,9 @@ if not args.torch:
 # Compile the MLIR code to use the PyTorch backend
 # We do not guarentee it works here as this is not the focus of this example
 if args.torch:
-    x = np.linspace(0,1,hypercube_size)
+    x_virtual = np.linspace(0,1,hypercube_size)
     # Generate some initial conditions
-    u0 = np.exp(-((x - 0.5) ** 2) / 0.01)
+    u0 = np.exp(-((x_virtual - 0.5) ** 2) / 0.01)
     np.save('advection_diffusion_initial_conditions.npy', u0)
 
     with open('advection_diffusion_pytorch_program.py', 'w') as output_file:
